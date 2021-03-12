b0d4a5cabdcfccb0751429929f13f2bbb73a8ba3
radan devist <radandevist@gmail.com>
2021-03-12 10:01:57 +0300

    remove completely material-ui

--- client/app/pages/Home/Home.tsx
+++ client/app/pages/Home/Home.tsx
@@ -1,34 +1,21 @@
 import React from "react";
-import useStyles from "./Home.style";
-import { Card, Typography, CardMedia, CardContent } from "@material-ui/core";
-import { Link } from "react-router-dom";
 import unicornImage
   from "@images/meritt-thomas-KTYjVDmN4A4-unsplash.jpg";
-
-// type HomeProps = {}
+import { Box } from "@chakra-ui/layout";
+import { Text, Image } from "@chakra-ui/react";
 
 const Home = (): JSX.Element => {
-  const classes = useStyles();
-
   return (
-    <Card className={classes.card}>
-      <Typography variant="h6" className={classes.title} >
-        Home Page
-      </Typography>
-      <CardMedia
-        className={classes.media}
-        image={unicornImage}
-        title="A fantasy unicorn"
-      />
-      <CardContent>
-        <span>Photo by <a href="https://unsplash.com/@merittthomas?utm_source=unsplash&amp;utm_medium=referral&amp;utm_content=creditCopyText">Meritt Thomas</a> on <a href="https://unsplash.com/s/photos/unicorn?utm_source=unsplash&amp;utm_medium=referral&amp;utm_content=creditCopyText">Unsplash</a></span>
-        <br/><br/>
-        <Typography variant="body2" component="p">
-          Welcome to This MERN Boilerplate, &nbsp;
-          <Link to="/form">visit another page</Link>
-        </Typography>
-      </CardContent>
-    </Card>
+    <Box>
+      <Text fontSize="5xl" color="whatsapp.500">Hey There!!</Text>
+      <Text fontSize="lg">
+        Just Switched to chakra-ui and react tailwind css!!
+      </Text>
+      <br/><br/>
+      <Box maxW="md" maxH="md" overflow="hidden">
+        <Image src={unicornImage}/>
+      </Box>
+    </Box>
   );
 };
 
